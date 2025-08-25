bd12c3ec8aa4c5d37ab737aaf41a6df74e9e92c4
MRV-dev <a.marv0903@gmail.com>
2025-08-26 00:34:45 +0800

    organized controllers

--- backend/middleware/multerConfig.js
+++ backend/middleware/multerConfig.js
@@ -4,12 +4,10 @@ const path = require('path');
 // Set up multer storage engine
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    // Set the correct path for the upload folder
-    const uploadPath = path.join(__dirname, '../uploads');  // Goes one level up to the root
-    cb(null, uploadPath);  // Ensure multer stores files in the uploads folder
+    const uploadPath = path.join(__dirname, '../uploads');  
+    cb(null, uploadPath);  
   },
   filename: (req, file, cb) => {
-    // Save file with a unique name (timestamp + original file extension)
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
