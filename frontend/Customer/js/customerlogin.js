f1e63c5ed6848d58ad9474326f160e93f357aee6
sheninna <sheninnajexellcaisip@gmail.com>
2025-09-02 16:54:42 +0800

    customer

--- frontend/Customer/js/customerlogin.js
+++ frontend/Customer/js/customerlogin.js
@@ -47,3 +47,22 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+
+document.querySelectorAll('.toggle-password').forEach(button => {
+  button.addEventListener('click', () => {
+    const targetId = button.getAttribute('data-target');
+    const input = document.getElementById(targetId);
+    const icon = button.querySelector('i');
+
+    if (input.type === 'passwordInput') {
+      input.type = 'text';
+      icon.classList.remove('fa-eye');
+      icon.classList.add('fa-eye-slash');
+      } else {
+        input.type = 'passwordInput';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+      }
+  });
+});
